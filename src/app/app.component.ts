8093ee18566ab016752b87cc4401ed0c06de4137
Rohan Kumar <rohankumar0095@gmail.com>
2019-12-03 19:59:36 +0530

    giphy changes as required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import {GiphyService} from "../app/giphy.service";
-
+import { Component, OnInit } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { GiphyService } from "../app/giphy.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  
-  title = 'giphy-search';
+  title = "giphy-search";
   items = [];
   animatedgif = "";
 
-  constructor(private giphy:GiphyService) {
+  constructor(private giphy: GiphyService) {}
+
+  ngOnInit() {}
+  clickgif(item) {
+    this.animatedgif = item.images.fixed_height.url;
   }
-
-
-  ngOnInit()
-  { 
-
+  giphySearch(str) {
+    this.giphy.getGiphy(str).subscribe(res => {
+      this.items = res["data"];
+    });
   }
-  giphySearch(str){
-    this.giphy.getGiphy(str).subscribe(res=> {
-      this.items = res['data'];
-
-    })
-
-  }
-
 }
